6875c476a7903f7d36c336591618cdffc16e2707
gitTongzhen <zai_rotk_xiao8@163.com>
2019-12-05 19:15:39 +0800

    新增自己动手的部分

--- test.py
+++ test.py
@@ -1,12 +1,85 @@
+
+# tensor 操作常见知识
 import torch
 # x[batch_size,channels,height_1,width_1]
 # conv[channels,output,height,width]
 # 输出 [2,8,7-2+1,3-3+1]
-x = torch.randn(2,1,7,7)
-print(type(x))
-conv = torch.nn.Conv2d(1,8,(3,3))
-maxpool = torch.nn.MaxPool2d(2,2)
-res = conv(x)
-res1 = maxpool(res)
-print(res1.shape)
-print(res.shape)
+x = torch.randn(2,1,3,3)
+x_noise = x.new(x.size()).zero_().long()
+print(x)
+print(x_noise.shape)
+print(x.device)
+print(x.dtype)
+print(x_noise[0][0][0][0])
+# conv = torch.nn.Conv2d(1,8,(3,3))
+# maxpool = torch.nn.MaxPool2d(2,2)
+# res = conv(x)
+# res1 = maxpool(res)
+# print(res1.shape)
+# print(res.shape)
+
+## cat 表示把矩阵拼起来
+## 后面的参数为0 的时候是按行拼起来，后面的参数为1的时候表示按列拼起来
+
+
+import torch
+
+A = torch.ones(2,3)
+b = torch.ones(4,3)
+C = torch.cat((A,b),0)
+print(C.size())
+
+
+# stack 增加新的维度进行堆叠
+
+a = torch.randn(1,2)
+b = torch.randn(1,2)
+c = torch.stack((a,b),0)
+print(a,b,c)
+
+## permute 适用于多维数据，更加灵活的transpose
+
+x = torch.randn(2,3,4)
+print(x.size())
+x_shape = x.permute(1,0,2)
+print(x_shape.size())
+
+## squeeze和unsqueeze
+# squeeze 是去掉元素数量为1的dim_n维度，同理unsqueeze(dim_n),增加dim_n维度
+import torch 
+b = torch.Tensor(2,1)
+b0 =b.unsqueeze(2)
+b_ = b.squeeze()
+print(b0.size())
+
+## reshape
+
+A = torch.randn(2,3,2)
+B = A.reshape(12,1)
+print(B)
+
+
+#torch.argmax(dim) 返回指定维度最大值的序号,dim是指定维度，dim = 0就是返回指定列的，为0就是指定行的
+## 同理对于torch.argmin(dim)也是适用的
+a = torch.Tensor([[1,5,5,2],
+                  [9,-6,2,8],
+                  [-3,7,-9,1]])
+b = torch.argmax(a,dim=0)
+print(b)
+print(b.shape)
+
+# tensor常见的取值操作:记住口诀，一行二列
+
+a = torch.Tensor([[1,5,5,2],
+                  [9,-6,2,8],
+                  [-3,7,-9,1]])
+b = a[:-1,:-2]
+print(b)
+
+# clamp 函数输出限制在最大值和最小值之间
+
+a = torch.Tensor([[1,5,5,2],
+                  [9,-6,2,8],
+                  [-3,7,-9,1]])
+b = torch.clamp(a,3,7)
+print(b)
--- 线性回归/LR.py
+++ 线性回归/LR.py
@@ -1,22 +1,35 @@
-import matplotlib
-# matplotlib.use('Qt4Agg')
-import matplotlib.pyplot as plt
-x = [6,8,10,14,18]
-y = [7,9,13,17.5,18]
 
+# ==============基于Scikit-learn接口的分类================
+from sklearn.datasets import load_iris
+import xgboost as xgb
+from xgboost import plot_importance
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-# plt.plot(x, y, 'k.')
-# plt.show()
-
-from sklearn import linear_model        #表示，可以调用sklearn中的linear_model模块进行线性回归。
+import pandas as pd
 import numpy as np
-model = linear_model.LinearRegression()
-X = np.array(x)[:, np.newaxis]
-Y = np.array(y)
+f = open('train_label.csv',encoding='UTF-8')
+csv_data = pd.read_csv(f)
 
-print(X)
-print(Y)
-model.fit(X, Y)
+print(type(csv_data))
 
-print(model.coef_)
-print(model.predict([[12]]))
+# # 加载样本数据集
+# iris = load_iris()
+# X,y = iris.data,iris.target
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234565) # 数据集分割
+# print("1111111",type(X_train))
+# # 训练模型
+# model = xgb.XGBClassifier(max_depth=5, learning_rate=0.1, n_estimators=160, silent=True, objective='multi:softmax')
+# model.fit(X_train, y_train)
+
+# # 对测试集进行预测
+# y_pred = model.predict(X_test)
+
+# # 计算准确率
+# accuracy = accuracy_score(y_test,y_pred)
+# print("accuarcy: %.2f%%" % (accuracy*100.0))
+
+# # 显示重要特征
+# plot_importance(model)
+# plt.show()
